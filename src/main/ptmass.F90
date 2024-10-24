34177b1af41ad6cbf8d4fe9accac107e63282adc
Christopher Russell <cmprussell@gmail.com>
2024-10-24 16:10:50 -0400

    bot fixes for ptmass.F90

--- src/main/ptmass.F90
+++ src/main/ptmass.F90
@@ -246,7 +246,7 @@ subroutine get_accel_sink_gas(nptmass,xi,yi,zi,hi,xyzmh_ptmass,fxi,fyi,fzi,phi, 
        !CuspMass = 6.e5 Msun
        !CuspAlpha1 = 1.6
        !CuspAlpha2 = 1.0
-       !if(r<=CuspRadius)
+       !if (r<=CuspRadius)
        !   mass_extra = CuspMass * (r/CuspRadius)^CuspAlpha1
        !else
        !   mass_extra = CuspMass * ((r/CuspRadius)^CuspAlpha2 * CuspAlpha1/CuspAlpha2 + 1.d0 - CuspAlpha1/CuspAlpha2)
@@ -335,7 +335,7 @@ subroutine get_accel_sink_gas(nptmass,xi,yi,zi,hi,xyzmh_ptmass,fxi,fyi,fzi,phi, 
        ! timestep is sqrt(separation/force)
        fonrmax = max(f1,f2,fonrmax)
        if (kappa) then
-          if(abs(bin_info(isemi,j))>tiny(f2)) then
+          if (abs(bin_info(isemi,j))>tiny(f2)) then
              bin_info(ipert,j) = bin_info(ipert,j) + f2
           endif
        endif
@@ -530,7 +530,7 @@ subroutine get_accel_sink_sink(nptmass,xyzmh_ptmass,fxyz_ptmass,phitot,dtsinksin
           !CuspMass = 6.e5 Msun
           !CuspAlpha1 = 1.6
           !CuspAlpha2 = 1.0
-          !if(r<=CuspRadius)
+          !if (r<=CuspRadius)
           !   mass_extra = CuspMass * (r/CuspRadius)^CuspAlpha1
           !else
           !   mass_extra = CuspMass * ((r/CuspRadius)^CuspAlpha2 * CuspAlpha1/CuspAlpha2 + 1.d0 - CuspAlpha1/CuspAlpha2)
@@ -908,7 +908,7 @@ subroutine ptmass_accrete(is,nptmass,xi,yi,zi,hi,vxi,vyi,vzi,fxi,fyi,fzi, &
  integer            :: i,ifail
  real               :: dx,dy,dz,r2,dvx,dvy,dvz,v2,hacc
  logical, parameter :: iofailreason=.false.
- !LOGICAL, PARAMETER :: iofailreason=.true.
+ !logical, parameter :: iofailreason=.true.
  integer            :: j
  real               :: mpt,tbirthi,drdv,angmom2,angmomh2,epart,dxj,dyj,dzj,dvxj,dvyj,dvzj,rj2,vj2,epartj
  logical            :: mostbound
@@ -925,8 +925,8 @@ subroutine ptmass_accrete(is,nptmass,xi,yi,zi,hi,vxi,vyi,vzi,fxi,fyi,fzi, &
     !   !write(iprint,"(/,a)") 'ptmass_accrete: FAILED: particle is not an accretable type'
     !   write(iprint,"(/,a,I3,L2)") 'ptmass_accrete: FAILED: particle is not an accretable type',itypei,is_accretable(itypei)
     return
-!ELSE
-!WRITE(*,*) 'ptmass_accrete: is accretable',itypei,is_accretable(itypei)
+    !else
+    !   write(*,*) 'ptmass_accrete: is accretable',itypei,is_accretable(itypei)
  endif
  !
  sinkloop : do i=is,nptmass
@@ -1007,14 +1007,14 @@ subroutine ptmass_accrete(is,nptmass,xi,yi,zi,hi,vxi,vyi,vzi,fxi,fyi,fzi, &
        case(2)
           write(iprint,"(/,a,Es9.2,a,Es9.2)") 'ptmass_accrete: FAILED: angular momentum is too large: ' &
                                               ,angmom2,' > ',angmomh2
-       !case(1)
-       !   write(iprint,"(/,a)") 'ptmass_accrete: FAILED: r2 > hacc**2'
+          !case(1)
+          !   write(iprint,"(/,a)") 'ptmass_accrete: FAILED: r2 > hacc**2'
        case(-1)
           write(iprint,"(/,a)") 'ptmass_accrete: PASSED indiscriminately: particle will be accreted'
        case(-2)
           write(iprint,"(/,a)") 'ptmass_accrete: PASSED: particle will be accreted'
-       !case default
-       !   write(iprint,"(/,a)") 'ptmass_accrete: FAILED: unknown reason'
+          !case default
+          !   write(iprint,"(/,a)") 'ptmass_accrete: FAILED: unknown reason'
        end select
     endif
     if (present(nfaili)) nfaili = ifail
