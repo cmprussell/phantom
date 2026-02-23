1db51161ebecccea2157861fa5d1dc384e65c922
Christopher Russell <cmprussell@gmail.com>
2026-02-22 23:57:34 -0500

    CWB: implemented kappa_follows_gas and radiative_inhibiton in extern_windaccel, including having these variables as logical parameters for options that are read in to describe the specific implementation of wind acceleration; removed some legacy lines of code from the gravity point-mass calculation that are not applicable to wind acceleration

--- src/main/extern_windaccel.f90
+++ src/main/extern_windaccel.f90
@@ -21,6 +21,7 @@ module extern_windaccel
  !--code input parameters: these are the default values
  !  and can be changed in the input file
  !
+ logical, public :: kappa_follows_gas=.true.,radiative_inhibition=.true.
 
  public :: get_windaccel_force
  public :: write_options_windaccel, read_options_windaccel
@@ -34,53 +35,24 @@ contains
 !  beta velocity law in the absence of companion stars
 !+
 !------------------------------------------------
-subroutine get_windaccel_force(xi,yi,zi,hi,fxi,fyi,fzi,phi,iwindorigj,jj)
+subroutine get_windaccel_force(xi,yi,zi,hi,fxi,fyi,fzi,phi,iwindorigj)!,jj)
  use part,   only:xyzmh_ptmass,nptmass,ihsoft,ihacc
  use part,   only:windaccel,ixantgrav,ikappac,ikappar,ivbeta
  use kernel, only:kernel_softening,radkern
 
  real, intent(in)    :: xi,yi,zi,hi
  real, intent(out)   :: fxi,fyi,fzi,phi
- integer, intent(in) :: iwindorigj,jj
+ integer, intent(in) :: iwindorigj!,jj
 
  real :: ddinv,xantigr,kappa_windaccel
  integer :: iorder(2),k
 
- !real                             :: ftmpxi,ftmpyi,ftmpzi
- real                             :: dx,dy,dz,rr2,ddr,dr3,f1,f2,pmassj
- real                             :: hsoft,hsoft1,hsoft21,q2i,qi,psoft,fsoft
- real                             :: fxj,fyj,fzj,dsx,dsy,dsz
- integer                          :: j
- logical                          :: tofrom,extrap,kappa
+ real    :: dx,dy,dz,rr2,ddr,dr3,f1,pmassj
+ integer :: j,nptmass_max
 
- !
- ! Determine if acceleration is from/to gas, or to gas
- !
- !if (present(pmassi) .and. present(fxyz_ptmass) .and. present(fonrmax)) then
- !   tofrom  = .true.
- !   fonrmax = 0.
- !else
- tofrom  = .false.
- !endif
-
- !! check if it is a force computed using Omelyan extrapolation method for FSI
- !if (present(extrapfac)) then
- !   extrap = .true.
- !else
- extrap = .false.
- !endif
-
- !if (present(bin_info)) then
- !   kappa = .true.
- !else
- kappa = .false.
- !endif
-
- !order in which ptmass computations are performed, which is the order in which kappa computations are performed
- !   needed when kappa is tied to gas, so only kappa(iwindorigi) needs to be computed
- !   not needed when kappa is tied to radiation field, in which kappa(1) and kappa(2) need to be computed
- !if (.TRUE.) then
- !if (.FALSE.) then
+ !Determine the order that ptmass computations are performed, which is the order that kappa computations are performed.
+ !   This is needed when kappa is tied to gas since the star iwindorigj needs to got first when computing kappa_windaccel.
+ !   This is not needed when kappa is tied to the radiation field, in which both kappa(1) and kappa(2) need to be computed.
  if (iwindorigj==1) then
     iorder(1) = 1
     iorder(2) = 2
@@ -97,7 +69,7 @@ subroutine get_windaccel_force(xi,yi,zi,hi,fxi,fyi,fzi,phi,iwindorigj,jj)
     print*, kind(iwindorigj),kind(2),kind(iorder)
     stop
  endif
- !else !this case statement was made when the above nested if statements were behaving unpredictably
+ !this case statement was made when the above nested if statements were behaving unpredictably
  !select case(iwindorigj)
  !   case(1)
  !      iorder(1) = 1
@@ -115,113 +87,79 @@ subroutine get_windaccel_force(xi,yi,zi,hi,fxi,fyi,fzi,phi,iwindorigj,jj)
  !      print*, kind(iwindorigj),kind(2),kind(iorder)
  !      stop
  !end select
- !endif
  !print*,'iwindorigj =',iwindorigj,', iorder = ',iorder
 
- !ftmpxi = 0.  ! use temporary summation variable
- !ftmpyi = 0.  ! (better for round-off, plus we need this bit of
- !ftmpzi = 0.  ! the force to calculate the dtphi timestep)
- !phi    = 0.
- !f2     = 0.
+ !only loop over one star if radiative inhibition is turned off; loop over all stars if radiative inhibition is turned on
+ if (radiative_inhibition) then
+    nptmass_max = nptmass
+ else
+    nptmass_max = 1
+ endif
+
  fxi = 0.
  fyi = 0.
  fzi = 0.
  phi = 0.
- f2  = 0.
 
- !do j=1,nptmass
- do k=1,nptmass
-    if (k>1) cycle !turn off wind accel on gas particles from non-originating (i.e. companion) star
+ do k=1,nptmass_max
     j = iorder(k)
     !if (j == iwindorigj) cycle !used for gravity/gravity-canceling tests
-    !if (extrap) then
-    !   dx     = xi - (xyzmh_ptmass(1,j) + extrapfac*fsink_old(1,j))
-    !   dy     = yi - (xyzmh_ptmass(2,j) + extrapfac*fsink_old(2,j))
-    !   dz     = zi - (xyzmh_ptmass(3,j) + extrapfac*fsink_old(3,j))
-    !else
-    dx     = xi - xyzmh_ptmass(1,j)
-    dy     = yi - xyzmh_ptmass(2,j)
-    dz     = zi - xyzmh_ptmass(3,j)
-    !endif
     pmassj = xyzmh_ptmass(4,j)
-    hsoft  = xyzmh_ptmass(ihsoft,j)
-    if (hsoft > 0.0) hsoft = max(hsoft,hi)
     if (pmassj < 0.0) cycle
 
-    rr2    = dx*dx + dy*dy + dz*dz + epsilon(rr2)
-!#ifdef FINVSQRT
-!    ddr    = finvsqrt(rr2)
-!#else
-    ddr    = 1./sqrt(rr2)
-!#endif
-    dsx = 0.
-    dsy = 0.
-    dsz = 0.
-    fxj = 0.
-    fyj = 0.
-    fzj = 0.
-    if (rr2 < (radkern*hsoft)**2) then
-       !
-       ! if the sink particle is given a softening length, soften the
-       ! force and potential if r < radkern*hsoft
-       !
-       hsoft1 = 1.0/hsoft
-       hsoft21= hsoft1**2
-       q2i    = rr2*hsoft21
-       qi     = sqrt(q2i)
-       call kernel_softening(q2i,qi,psoft,fsoft)  ! Note: psoft < 0
+    dx = xi - xyzmh_ptmass(1,j)
+    dy = yi - xyzmh_ptmass(2,j)
+    dz = zi - xyzmh_ptmass(3,j)
 
-       ! acceleration of gas due to point mass particle
-       f1     = pmassj*fsoft*hsoft21*ddr
-       !ftmpxi = ftmpxi - dx*f1
-       !ftmpyi = ftmpyi - dy*f1
-       !ftmpzi = ftmpzi - dz*f1
-       !phi    = phi + pmassj*psoft*hsoft1  ! potential (spline-softened)
+    rr2 = dx*dx + dy*dy + dz*dz + epsilon(rr2)
+    ddr = 1./sqrt(rr2)
+    dr3 = ddr*ddr*ddr
 
-       !! acceleration of sink from gas
-       !if (tofrom) f2 = pmassi*fsoft*hsoft21*ddr
-    else
-       ! no softening on the sink-gas interaction
-       dr3  = ddr*ddr*ddr
-
-       ! acceleration of gas due to point mass particle
-       f1     = pmassj*dr3
-
-       !! acceleration of sink from gas
-       !if (tofrom) f2 = pmassi*dr3
-
-       !ftmpxi = ftmpxi - dx*f1
-       !ftmpyi = ftmpyi - dy*f1
-       !ftmpzi = ftmpzi - dz*f1
-       !phi    = phi    - pmassj*ddr      ! potential (GM/r)
-    endif
+    ! acceleration of gas due to point mass particle
+    !This is the gravity component, which is how the wind acceleration is scaled.
+    f1 = pmassj*dr3
 
     !wind acceleration
-    !!!!if (k==1) then !use this if statement for no radiative inhibition; comment out this if statement for using radiative inhibition
-    if (k==1) then !use this if statement for radiative inhibition where kappa follows the gas
-       !kappa follows gas
-       ddinv = xyzmh_ptmass(ihacc,iwindorigj) * ddr ! Radius/distance = R/r
-       !kappa follows radiation field
-       !ddinv = xyzmh_ptmass(ihacc,j) * ddr ! Radius/distance = R/r
+    !!!!!if (k==1) then !use this if statement for no radiative inhibition; comment out this if statement for using radiative inhibition
+    !if (k==1) then !use this if statement for radiative inhibition where kappa follows the gas
+    !   !kappa follows gas
+    !   ddinv = xyzmh_ptmass(ihacc,iwindorigj) * ddr ! Radius/distance = R/r
+    !   !kappa follows radiation field
+    !   !ddinv = xyzmh_ptmass(ihacc,j) * ddr ! Radius/distance = R/r
+    !   if (ddinv<1.d0) then
+    !      !kappa follows gas
+    !      kappa_windaccel = windaccel(ikappac,iwindorigj) + &
+    !                        windaccel(ikappar,iwindorigj)*(1.0d0-ddinv)**(2.d0*windaccel(ivbeta,iwindorigj)-1.d0)
+    !      !kappa follows radiation field
+    !      !kappa_windaccel = windaccel(ikappac,j) + &
+    !      !                  windaccel(ikappar,j)*(1.0d0-ddinv)**(2.d0*windaccel(ivbeta,j)-1.d0)
+    !      !elseif (ddinv>1.0d0) then !use for debugging of exact location of particle; not needed for regular use
+    !      !kappa_windaccel = 0.d0
+    !      !print*,'particle originating from star ',iwindorigj,' is within the radius of star ',j,': ',xyzmh_ptmass(ihacc,iwindorigj),rr2
+    !   else
+    !      kappa_windaccel = 0.d0
+    !      !print*,'particle originating from star ',iwindorigj,' is exactly at the radius of star ',j,': ',xyzmh_ptmass(ihacc,iwindorigj),rr2
+    !   endif
+    !endif
+    !Compute kappa_windaccel for the first star or if kappa follows the radiation field.
+    !If kappa follows the gas, then kappa_windaccel from k=1 is used for k>1 as well,
+    !   so kappa_windaccel does not need to be recomputed.
+    if (k==1 .or. (.not.kappa_follows_gas)) then
+       ddinv = xyzmh_ptmass(ihacc,j) * ddr ! radius/distance = R/r
        if (ddinv<1.d0) then
           !kappa follows gas
-          kappa_windaccel = windaccel(ikappac,iwindorigj) + &
-                            windaccel(ikappar,iwindorigj)*(1.0d0-ddinv)**(2.d0*windaccel(ivbeta,iwindorigj)-1.d0)
-          !kappa follows radiation field
-          !kappa_windaccel = windaccel(ikappac,j) + &
-          !                  windaccel(ikappar,j)*(1.0d0-ddinv)**(2.d0*windaccel(ivbeta,j)-1.d0)
-       elseif (ddinv>1.0d0) then
-          kappa_windaccel = 0.d0
-          !print*,'particle originating from star ',iwindorigj,' is within the radius of star ',j,': ',xyzmh_ptmass(ihacc,iwindorigj),rr2
+          kappa_windaccel = windaccel(ikappac,j) + &
+                            windaccel(ikappar,j)*(1.0d0-ddinv)**(2.d0*windaccel(ivbeta,j)-1.d0)
+          !elseif (ddinv>1.0d0) then !use for debugging of exact location of particle; not needed for regular use
+          !kappa_windaccel = 0.d0
+          !print*,'particle originating from star ',iwindorigj,' is within the radius of star ',j,': ',xyzmh_ptmass(ihacc,j),rr2
        else
           kappa_windaccel = 0.d0
-          !print*,'particle originating from star ',iwindorigj,' is exactly at the radius of star ',j,': ',xyzmh_ptmass(ihacc,iwindorigj),rr2
+          !print*,'particle originating from star ',iwindorigj,' is exactly at the radius of star ',j,': ',xyzmh_ptmass(ihacc,j),rr2
        endif
     endif
     xantigr = windaccel(ixantgrav,j)*kappa_windaccel
-    !ftmpxi = ftmpxi + xantigr*dx*f1
-    !ftmpyi = ftmpyi + xantigr*dy*f1
-    !ftmpzi = ftmpzi + xantigr*dz*f1
+
     fxi = fxi + xantigr*dx*f1
     fyi = fyi + xantigr*dy*f1
     fzi = fzi + xantigr*dz*f1
@@ -273,46 +211,7 @@ subroutine get_windaccel_force(xi,yi,zi,hi,fxi,fyi,fzi,phi,iwindorigj,jj)
     !!      akappa(ipart) = 0.0
     !!   endif
     !!endif
-
-    !if (tofrom) then
-    !   ! backreaction of gas onto sink
-    !   fxyz_ptmass(1,j) = fxyz_ptmass(1,j) + dx*f2 + fxj*pmassi/pmassj
-    !   fxyz_ptmass(2,j) = fxyz_ptmass(2,j) + dy*f2 + fyj*pmassi/pmassj
-    !   fxyz_ptmass(3,j) = fxyz_ptmass(3,j) + dz*f2 + fzj*pmassi/pmassj
-    !
-    !   ! backreaction torque of gas onto oblate sink
-    !   dsdt_ptmass(1,j) = dsdt_ptmass(1,j) + pmassi*dsx
-    !   dsdt_ptmass(2,j) = dsdt_ptmass(2,j) + pmassi*dsy
-    !   dsdt_ptmass(3,j) = dsdt_ptmass(3,j) + pmassi*dsz
-    !
-    !   ! timestep is sqrt(separation/force)
-    !   fonrmax = max(f1,f2,fonrmax)
-    !   if (kappa) then
-    !      if (abs(bin_info(isemi,j))>tiny(f2)) then
-    !         bin_info(ipert,j) = bin_info(ipert,j) + f2
-    !      endif
-    !   endif
-    !endif
  enddo
- !
- ! external force timestep based on sqrt(phi)/accel
- !
- !if (present(dtphi2)) then
- !   if (abs(phi) > epsilon(phi)) then
- !      f2     = ftmpxi*ftmpxi + ftmpyi*ftmpyi + ftmpzi*ftmpzi
- !      !dtphi is sqrt of this, but for optimisation we take the sqrt outside of
- !      !the loop
- !      dtphi2 = dtfacphi2*abs(phi)/f2
- !   else
- !      dtphi2 = huge(dtphi2)
- !   endif
- !endif
- !
- ! add temporary sums to existing force on gas particle
- !
- !fxi = fxi + ftmpxi
- !fyi = fyi + ftmpyi
- !fzi = fzi + ftmpzi
 
 end subroutine get_windaccel_force
 
@@ -322,8 +221,11 @@ end subroutine get_windaccel_force
 !+
 !-----------------------------------------------------------------------
 subroutine write_options_windaccel(iunit)
- !use infile_utils, only:write_inopt
+ use infile_utils, only:write_inopt
  integer, intent(in) :: iunit
+
+ call write_inopt(kappa_follows_gas,'kappa_follows_gas','kappa follows gas or radiation field',iunit)
+ call write_inopt(radiative_inhibition,'radiative_inhibition','radiative inhibition on or off',iunit)
 
 end subroutine write_options_windaccel
 
@@ -333,16 +235,27 @@ end subroutine write_options_windaccel
 !+
 !-----------------------------------------------------------------------
 subroutine read_options_windaccel(name,valstring,imatch,igotall,ierr)
- !use io,      only:fatal
+ use io,      only:fatal
  character(len=*), intent(in)  :: name,valstring
  logical,          intent(out) :: imatch,igotall
  integer,          intent(out) :: ierr
- !integer, save :: ngot = 0
+ integer, save :: ngot = 0
  !character(len=30), parameter :: label = 'read_options_windaccel'
 
- igotall=.true.
- imatch = .false.
- ierr=0
+ igotall = .false.
+ imatch = .true.
+ select case(trim(name))
+ case('kappa_follows_gas')
+    read(valstring,*,iostat=ierr) kappa_follows_gas
+    ngot = ngot + 1
+ case('radiative_inhibition')
+    read(valstring,*,iostat=ierr) radiative_inhibition
+    ngot = ngot + 1
+ case default
+    imatch = .false.
+ end select
+ 
+ igotall = (ngot >= 2)
 
 end subroutine read_options_windaccel
 
